1c45fa5268875bdb1d954f414af505be483b07f2
Matthew Berwick <msberwick@uncg.edu>
2025-06-02 21:33:00 -0400

    Create the Game Mater API files. This PR fixes #18

---
+++ backend-api/src/main/java/csc340Team10/DnDGroupFinder/gamemaster/GameMaster.java
@@ -0,0 +1,73 @@
+package csc340Team10.DnDGroupFinder.gamemaster;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "gamemasters") 
+public class GameMaster {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long GMID;
+    @NonNull
+    @Column(unique = true)
+    private String email;
+    @NonNull
+    @Column(unique = true)
+    private String username;
+    @NonNull
+    private String password;
+
+    public GameMaster() {
+    }
+
+    public GameMaster(Long GMID, String email, String username, String password) {
+        this.GMID = GMID;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public GameMaster(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getGMID() {
+        return GMID;
+    }
+
+    public void setGMID(Long id) {
+        this.GMID = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ backend-api/src/main/java/csc340Team10/DnDGroupFinder/gamemaster/GameMasterService.java
@@ -0,0 +1,59 @@
+package csc340Team10.DnDGroupFinder.gamemaster;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class GameMasterService {
+    
+    @Autowired
+    private GameMasterRepository gameMasterRepository;
+
+    public Object getAllGameMasters() {
+        return gameMasterRepository.findAll();
+    }
+
+    public GameMaster getGameMastersById(@PathVariable long GMID) {
+        return gameMasterRepository.findById(GMID).orElse(null);
+    }
+
+    public GameMaster addGameMaster(GameMaster gameMaster) {
+        return gameMasterRepository.save(gameMaster);
+    }
+
+    public GameMaster updateGameMaster(Long GMID, GameMaster gameMaster) {
+        return gameMasterRepository.save(gameMaster);
+    }
+
+    public void deleteGameMaster(Long GMID) {
+        gameMasterRepository.deleteById(GMID);
+    }
+
+    public String writeJson(GameMaster gameMaster) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("game-masters.json"), gameMaster);
+            return "Game Master written to JSON file successfully";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error writing turtle to JSON file";
+        }
+    }
+
+    public Object readJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(new File("game-masters.json"), GameMaster.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
