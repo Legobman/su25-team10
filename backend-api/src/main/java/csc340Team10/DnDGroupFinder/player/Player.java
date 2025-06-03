37ecfc882d21faf90fc9076d866093ca7cca5393
Matthew Berwick <msberwick@uncg.edu>
2025-06-03 06:07:05 -0400

    Added basic Player API files

---
+++ backend-api/src/main/java/csc340Team10/DnDGroupFinder/player/Player.java
@@ -0,0 +1,75 @@
+package csc340Team10.DnDGroupFinder.player;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "players") 
+public class Player {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long playerID;
+    @NonNull
+    @Column(unique = true)
+    private String email;
+    @NonNull
+    @Column(unique = true)
+    private String username;
+    @NonNull
+    private String password;
+
+    public Player() {
+    }
+
+    public Player(String email, String password, Long playerID, String username) {
+        this.email = email;
+        this.password = password;
+        this.playerID = playerID;
+        this.username = username;
+    }
+
+    public Player(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Long getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(Long playerID) {
+        this.playerID = playerID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
