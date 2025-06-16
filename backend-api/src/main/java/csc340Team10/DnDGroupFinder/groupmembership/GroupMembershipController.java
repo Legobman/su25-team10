7c6549369d8218f938333b1dd13a5ebb02102295
Matthew Berwick <msberwick@uncg.edu>
2025-06-16 13:54:06 -0400

    Setup API for looking up group memberships

--- backend-api/src/main/java/csc340Team10/DnDGroupFinder/groupmembership/GroupMembershipController.java
+++ backend-api/src/main/java/csc340Team10/DnDGroupFinder/groupmembership/GroupMembershipController.java
@@ -2,11 +2,10 @@ package csc340Team10.DnDGroupFinder.groupmembership;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
@@ -15,38 +14,61 @@ public class GroupMembershipController {
     private GroupMembershipService groupMembershipService;
 
     @GetMapping("/memberships")
-    public Object getAllMemberships() {
-        return groupMembershipService.getAllMemberships();
+    public Object getAllMemberships(Model model) {
+        //model.addAttribute("turtlesList", groupMembershipService.getAllMemberships());
+        //model.addAttribute("title", "All Turtles");
+        return "groupmembership-search";
     }
 
     @GetMapping("/memberships/{id}")
-    public GroupMembership getMembershipById(@PathVariable long id) {
-        return groupMembershipService.getMembershipById(id);
+    public Object getMembershipById(@PathVariable long id, Model model) {
+        model.addAttribute("groupmembership", groupMembershipService.getMembershipById(id));
+        model.addAttribute("title", "Group Membership #: " + id);
+        return "groupmembership-details";
     }
 
     @GetMapping("/memberships/find/{playerid}")
-    public Object findPlayerGroups(@PathVariable Long playerid) {
-        return groupMembershipService.findPlayerGroups(playerid);
+    public Object findPlayerGroups(@PathVariable Long playerid, Model model) {
+        model.addAttribute("groupMembershipList", groupMembershipService.findPlayerGroups(playerid));
+        model.addAttribute("title", "Group Membership by Player");
+        return "groupmembership-list";
+    }
+
+    @GetMapping("/memberships/createForm")
+    public Object showCreateForm(Model model) {
+        GroupMembership membership = new GroupMembership();
+        model.addAttribute("groupMembership", membership);
+        model.addAttribute("title", "Create New Group Membership");
+        return "groupmembership-create";
     }
 
     @PostMapping("/memberships")
-    public Object addMembership(@RequestBody GroupMembership membership) {
-        return groupMembershipService.addMembership(membership);
+    public Object addMembership(GroupMembership membership) {
+        GroupMembership newMembership = groupMembershipService.addMembership(membership);
+        return "redirect:/memberships/" + newMembership.getMembershipID();
     }
 
-    @PutMapping("/memberships/{id}")
-    public GroupMembership updateMembership(@PathVariable Long id, @RequestBody GroupMembership membership) {
+    @GetMapping("/memberships/updateForm/{id}")
+    public Object showUpdateForm(@PathVariable Long id, Model model) {
+        GroupMembership membership = groupMembershipService.getMembershipById(id);
+        model.addAttribute("groupMembership", membership);
+        model.addAttribute("title", "Update Group Membership: " + id);
+        return "groupmembership-update";
+    }
+
+    @PostMapping("/memberships/update/{id}")
+    public Object updateMembership(@PathVariable Long id, GroupMembership membership) {
         groupMembershipService.updateMembership(id, membership);
-        return groupMembershipService.getMembershipById(id);
+        return "redirect:/memberships/" + id;
     }
 
-    @DeleteMapping("/memberships/{id}")
+    @GetMapping("/memberships/delete/{id}")
     public Object deleteMembership(@PathVariable Long id) {
         groupMembershipService.deleteMembership(id);
-        return groupMembershipService.getAllMemberships();
+        return "redirect:/memberships";
     }
 
-    @DeleteMapping("/memberships/{groupid}/{playerid}")
+    @GetMapping("/memberships/{groupid}/{playerid}")
     public Object kickPlayer(@PathVariable Long groupid, @PathVariable Long playerid) {
         groupMembershipService.kickPlayer(groupid, playerid);
         return groupMembershipService.getAllMemberships();
