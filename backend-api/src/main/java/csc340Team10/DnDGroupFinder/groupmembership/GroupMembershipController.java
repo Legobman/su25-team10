2774e7fa458c0e63f45bbdaa5ca5b92c5e4dfe79
Matthew Berwick <msberwick@uncg.edu>
2025-06-16 15:11:43 -0400

    Finished page for looking up player's game groups. This PR fixes #26

--- backend-api/src/main/java/csc340Team10/DnDGroupFinder/groupmembership/GroupMembershipController.java
+++ backend-api/src/main/java/csc340Team10/DnDGroupFinder/groupmembership/GroupMembershipController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GroupMembershipController {
@@ -28,7 +29,7 @@ public class GroupMembershipController {
     }
 
     @GetMapping("/memberships/find")
-    public Object findPlayerGroups(@PathVariable Long playerid, Model model) {
+    public Object findPlayerGroups(@RequestParam("playerID") Long playerid, Model model) {
         model.addAttribute("groupMembershipList", groupMembershipService.findPlayerGroups(playerid));
         model.addAttribute("title", "Group Membership by Player");
         return "groupmembership-list";
