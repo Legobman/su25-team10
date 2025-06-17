4cb66b7d77627bff1e9fd5dbc9f3bbf979eb4e7d
Matthew Berwick <msberwick@uncg.edu>
2025-06-17 12:58:46 -0400

    Implemented kick method for Game Master.

--- backend-api/src/main/java/csc340Team10/DnDGroupFinder/groupmembership/GroupMembershipRepository.java
+++ backend-api/src/main/java/csc340Team10/DnDGroupFinder/groupmembership/GroupMembershipRepository.java
@@ -3,16 +3,20 @@ package csc340Team10.DnDGroupFinder.groupmembership;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface GroupMembershipRepository extends JpaRepository<GroupMembership, Long>{
 
-    @Query(value = "delete from groupmemberships gms where gms.playerID = ?1 and gms.groupID = ?1", nativeQuery = true)
-    List<GroupMembership> kickPlayer(long groupID, long playerID);
+    @Modifying
+    @Transactional
+    @Query(value = "delete from groupmemberships gms where gms.playerID = ?2 and gms.groupID = ?1", nativeQuery = true)
+    void kickPlayer(long groupID, long playerID);
 
-    @Query(value = "select gms.* FROM groupmemberships gms JOIN players p ON gms.playerID = p.playerID WHERE p.username = ?1", nativeQuery = true)
+    @Query(value = "select gms.* from groupmemberships gms join players p on gms.playerID = p.playerID where p.username = ?1", nativeQuery = true)
     List<GroupMembership> findPlayerGroups(String username);
 
 }
